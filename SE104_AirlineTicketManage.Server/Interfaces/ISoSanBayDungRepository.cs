10c2d70f0c36b12084af06bd75661e330b9f3553
Nguyễn Hoàng Phúc <hoangphucseiza@gmail.com>
2024-05-18 05:39:25 +0700

    Api GetThoiGianBayToiThieu

--- SE104_AirlineTicketManage.Server/Interfaces/ISoSanBayDungRepository.cs
+++ SE104_AirlineTicketManage.Server/Interfaces/ISoSanBayDungRepository.cs
@@ -5,7 +5,10 @@
         bool SoSanBayDungExists(string maSBDi, string maSBDen);
         int GetSoSBDungToiDa(string maSBDi, string maSBDen);
 
+        int GetThoiGianBayToiThieu(string maSBDi, string maSBDen);
+
         bool UpdateSoSBDungToiDa(string maSB1, string maSB2, int SoSBDungMax);
+
         bool Save();
     }
 }
